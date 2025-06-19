674ad8e2ad5a57130d3119c23ac7713ead56633d
Yagnik Patel <yagnikpatel5253@gmail.com>
2025-06-19 21:17:45 +0530

    Initial Commit

---
+++ ChatPrototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatPrototype
+//
+//  Created by Yagnik Patel on 19/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
